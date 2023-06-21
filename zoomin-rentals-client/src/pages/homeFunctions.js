a2ef2a917ccddd5d1f8caab9234058f00e4a6467
domce69 <tomm.andraos@gmail.com>
2023-06-21 14:14:45 +1000

    added search functionality

--- zoomin-rentals-client/src/pages/homeFunctions.js
+++ zoomin-rentals-client/src/pages/homeFunctions.js
@@ -1,19 +1,30 @@
 //get user by id in local storage 
-
 $(document).ready(function () {
-    var id = localStorage.getItem("loggedin");
-    $.ajax({
-		url: "http://localhost:9999/customers/" + id,
-		type: "GET",
-		dataType: "json",
-		contentType: "application/json",
-		success: function (data) {
-		 $("a#profilebtn").html(data.name);
-         $("a#logbtn").hide();
-		},
-		error: function () {
-		  alert("FAILURE!");
-		},
-	  });
+    if(localStorage.getItem("loggedin") === null){
+		alert("Login to book today");
+	} else {
+		var id = localStorage.getItem("loggedin");
+		$.ajax({
+			url: "http://localhost:9999/customers/" + id,
+			type: "GET",
+			dataType: "json",
+			contentType: "application/json",
+			success: function (data) {
+			 $("a#profilebtn").html(data.name);
+			 $("a#logbtn").hide();
+			},
+			error: function () {
 
+			},
+		  });
+	}
+  });
+
+  $(document).ready(function () {
+	$("#searchbtn").on("click", function () {
+	  var selected = $('#seatoptions').find(":selected").text();
+
+	  localStorage.setItem("seatcount", selected);
+	  location.href = "searchResult.html";
+	});
   });
