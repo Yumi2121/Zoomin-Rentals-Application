f321721b133569f94bdf974bb3efe78499401807
Thomas Andraos <104111958+domce69@users.noreply.github.com>
2023-06-23 11:53:10 +1000

    added profile button functionality

--- zoomin-rentals-client/src/pages/searchFunctions.js
+++ zoomin-rentals-client/src/pages/searchFunctions.js
@@ -1,4 +1,25 @@
 $(document).ready(function () {
+    if(localStorage.getItem("loggedin") === null){
+		alert("Login to book today");
+	} else {
+		var id = localStorage.getItem("loggedin");
+		$.ajax({
+			url: "http://localhost:9999/customers/" + id,
+			type: "GET",
+			dataType: "json",
+			contentType: "application/json",
+			success: function (data) {
+			 $("a#profilebtn").html(data.name);
+			 $("a#logbtn").hide();
+			},
+			error: function () {
+
+			},
+		  });
+	}
+  });
+
+$(document).ready(function () {
   var seats = localStorage.getItem("seatcount");
   var pdate = localStorage.getItem("pdate");
   var rdate = localStorage.getItem("rdate");
